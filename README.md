aa34d2c42cdcce4b844931e99d9d4b61246dee8f
alphaArgon <49616104+alphaArgon@users.noreply.github.com>
2021-09-24 16:40:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# 多行夹注 / Cutting Notes
+
+在网页中创建多行夹注。  
+Create cutting notes in your web pages.
+
+## 使用方法 / Usage
+
+引入 `cutting-note.js`，然后…  
+Includes `cutting-note.js` and then...
+
+```js
+makeCuttingNote(element, lineCount?, innerHTML?, averageCharacterWidth?);
+```
+
+如果内容需要更新，你需要手动再次调用这个函数。  
+You need to call this function manually if the contents need to be updated.
+
+## 演示 / Demos
+
+- [Demo A](./demos/demo.a.html)
+- [Demo B](./demos/demo.a.html)
+
+## 参数 / Parameters
+
+- `element: HTMLElement`
+
+   要对其内部作夹注的 HTML 元素。  
+   An HTML element to be made a cutting note inside.
+
+-  `lineCount: number`
+
+   夹注行数，默认值为 2。  
+   The max line count per segment in the cutting note. The default value is `2`.
+
+- `innerHTML: string`
+
+   如果不提供，将以 `element.textContent` 作为内部内容。  
+   If not provided, `element.textContent` will be the inner content of the cutting note.
+
+- `averageCharacterWidth: number`
+
+   字符平均宽度，默认值为 `0.8`。对于汉字，可以传以 `1`。  
+   The average width of the characters. The default value is `0.8`. For common Western text, you can pass `0.6`.
+
+## 讨论 / Discussion
+
+### 性能 / Performance
+
+**慢**。如果你的网页中有很多夹注，考虑使用 `setTimeOut` 或者懒加载。异步渲染注意夹注先后顺序。  
+**Slow**. Consider making use of `setTimeOut` or lazy loading if there are lots of cutting notes in a webpage. Take care of the order of notes in async rendering.
+
+### 富文本 / Rich Texts
+
+如果不传入 `innerHTML`，`makeCuttingNote` 将使用 `element` 的 `textContent` 作为夹注内容。如果你不需要富文本，可以一直忽略 `innerHTML`，因为该函数虽然改变了内部 DOM 结构，但并不会改变文字内容。  
+If `innerHTML` is not passed, `makeCuttingNote` will use `textContent` of `element` as the note content. If you do not need rich texts, you can always omit `innerHTML` because though this function may change the DOM structure of an element, the text content of it stays the same.
+
+注意，耗时与 `innerHTML` 的复杂度呈正相关，如果你确信某些子元素内部不会换行，可以考虑用等宽度的文本代替该元素，在该函数执行结束后再从 `element.innerHTML` 替换回来。  
+Note that the time-consuming and the complexity of `innerHTML` have a positive correlation. If a child element will never wrap, you may replace it with a text placeholder of the same width, and then replace the placeholder back in `element.innerHTML` after the function is executed.
+
+### 耐操性 / Robustness
+
+如果输入看起来很正常，那么输出也不会很糟糕。注意，父容器的边界必须是矩形。另外，如果有偏离 inline box 较大的内部元素，可能会干扰行数判断。  
+The normal input, the normal out. Note that the internal boundary of the parent element must be a rectangle. And, an inner element derailed greatly from the inline box may interfere with the detection of the line count.
+
+在 Internet Explorer 下不稳定。  
+Not stable in Internet Explorer.
---
+++ cutting-notes.js
@@ -0,0 +1,300 @@
+function makeCuttingNote(element, lineCount = 2, innerHTML, averageCharacterWidth = 0.8) {
+    let parent = element.parentElement;
+    let parentStyle = getComputedStyle(parent);
+    let elementStyle = getComputedStyle(element);
+
+    let isVertical = ["vertical-rl", "vertical-lr", "tb", "tb-rl", "tb-lr"].indexOf(parentStyle.writingMode) > -1;
+    if(isVertical) {
+        var kBlockSize = "width";
+        var kInlineSize = "height";
+        var kOffsetInlineStart = "offsetTop";
+        var kClientBlockSize = "clientWidth";
+        var kClientInlineSize = "clientHeight";
+        var kScrollBlockSize = "scrollWidth";
+        var kScrollInlineSize = "scrollHeight";
+        var kPaddingInlineStart = "paddingTop";
+        var kPaddingInlineEnd = "paddingBottom";
+        var kBorderInlineStart = "borderTop";
+        var kBorderInlineEnd = "borderBottom";
+        var kCSSMarginInlineEnd = "margin-bottom";
+    } else {
+        var kBlockSize = "height";
+        var kInlineSize = "width";
+        var kOffsetInlineStart = "offsetLeft";
+        var kClientBlockSize = "clientHeight";
+        var kClientInlineSize = "clientWidth";
+        var kScrollBlockSize = "scrollHeight";
+        var kScrollInlineSize = "scrollWidth";
+        var kPaddingInlineStart = "paddingLeft";
+        var kPaddingInlineEnd = "paddingRight";
+        var kBorderInlineStart = "borderLeft";
+        var kBorderInlineEnd = "borderRight";
+        var kCSSMarginInlineEnd = "margin-right";
+    }
+
+    let blockSizeTestText;
+    if(innerHTML === undefined) {
+        blockSizeTestText = element.textContent = element.textContent;
+        innerHTML = element.innerHTML;
+    } else {
+        blockSizeTestText = element.textContent;
+    }
+
+    element.innerHTML = "";
+
+    let fontSize = parseInt(elementStyle.fontSize);
+
+    let maxInlineSize = parent[kClientInlineSize]
+        - (parseInt(parentStyle[kPaddingInlineStart]) || 0)
+        - (parseInt(parentStyle[kPaddingInlineEnd]) || 0);
+
+    if(maxInlineSize <= fontSize) {
+        element.innerHTML = innerHTML;
+        return;
+    }
+
+    let firstLineStartPadding =
+        + (parseInt(elementStyle[kPaddingInlineStart]) || 0)
+        + (parseInt(elementStyle[kBorderInlineStart]) || 0);
+
+    let firstLineEndPadding =
+        + (parseInt(elementStyle[kPaddingInlineEnd]) || 0)
+        + (parseInt(elementStyle[kBorderInlineEnd]) || 0);
+
+    let firstLineMaxInlineSize = maxInlineSize - (element.offsetParent === parent
+        ? element[kOffsetInlineStart]
+        : element[kOffsetInlineStart] - parent[kOffsetInlineStart])
+        + (parseInt(parentStyle[kPaddingInlineStart]) || 0)
+        - firstLineStartPadding;
+
+    if(firstLineMaxInlineSize <= fontSize) {firstLineMaxInlineSize = maxInlineSize - firstLineStartPadding;}
+
+    let testElement = document.createElement("c");
+    testElement.innerHTML = blockSizeTestText;
+    for(let i = 1; i < lineCount; ++i) {testElement.innerHTML += "<br>" + blockSizeTestText;}
+    testElement.setAttribute("style", "position: fixed; visibility: hidden; white-space: nowrap;");
+    element.appendChild(testElement);
+
+    let stepScale = 1.25;
+    let blockSize = testElement[kClientBlockSize];
+    let blockSizeTolerance = fontSize / 2;
+
+    if((blockSize + blockSizeTolerance) / lineCount <= fontSize) {
+        element.innerHTML = innerHTML;
+        return;
+    }
+
+    testElement.style[kBlockSize] = blockSize + "px";
+    testElement.style.display = "block";
+    testElement.style.whiteSpace = "";
+    testElement.innerHTML = innerHTML;
+
+    let fragment = document.createDocumentFragment();
+
+    let isFirstLine = true;
+    let hasAppendSegment = false;
+
+    while(true) {
+        let initialTestInlineSize = isFirstLine
+            ? firstLineMaxInlineSize : hasAppendSegment
+            ? maxInlineSize
+            : maxInlineSize - firstLineStartPadding;
+        testElement.style[kInlineSize] = initialTestInlineSize + "px";
+
+        let inlineSize;
+        let range = document.createRange();
+        range.setStart(testElement, 0);
+
+        if(testElement[kScrollBlockSize] <= blockSize + blockSizeTolerance) {
+            range.setEnd(testElement, testElement.childNodes.length);
+            let textLength = testElement.textContent.length;
+            inlineSize = Math.min(initialTestInlineSize, textLength * fontSize * averageCharacterWidth / lineCount);
+            let maxInlineSize = Math.min(initialTestInlineSize, inlineSize * stepScale + 1) | 0;
+            let minInlineSize = Math.min(maxInlineSize, Math.max(1, inlineSize / stepScale - 1)) | 0;
+
+            let needsTestMin = true;
+
+            while(minInlineSize < maxInlineSize) {
+                testElement.style[kInlineSize] = maxInlineSize + "px";
+                if(testElement[kScrollBlockSize] <= blockSize + blockSizeTolerance
+                && testElement[kScrollInlineSize] <= maxInlineSize) {
+                    break;
+                } else {
+                    minInlineSize = maxInlineSize;
+                    maxInlineSize = Math.min(initialTestInlineSize, maxInlineSize * stepScale + 1) | 0;
+                    needsTestMin = false;
+                }
+            }
+
+            while(needsTestMin && minInlineSize < maxInlineSize) {
+                testElement.style[kInlineSize] = minInlineSize + "px";
+                if(testElement[kScrollBlockSize] > blockSize + blockSizeTolerance
+                || testElement[kScrollInlineSize] > minInlineSize) {
+                    break;
+                } else {
+                    maxInlineSize = minInlineSize;
+                    minInlineSize = Math.max(1, minInlineSize / stepScale - 1) | 0;
+                }
+            }
+
+            inlineSize = minInlineSize;
+
+            while(minInlineSize < maxInlineSize) {
+                inlineSize = Math.ceil((minInlineSize + maxInlineSize) / 2);
+                if(inlineSize === maxInlineSize) {break;}
+
+                testElement.style[kInlineSize] = inlineSize + "px";
+                if(testElement[kScrollBlockSize] > blockSize + blockSizeTolerance
+                || testElement[kScrollInlineSize] > inlineSize) {
+                    minInlineSize = inlineSize;
+                } else {
+                    maxInlineSize = inlineSize;
+                }
+            }
+
+        } else {
+            inlineSize = initialTestInlineSize;
+            let node = testElement;
+            let textLengthBefore = 0;
+            let approximateTextLength = inlineSize * lineCount / averageCharacterWidth / fontSize | 0;
+
+            while(true) {
+                let totalOffset;
+                let offset;
+                let childNodesTextLengths;
+
+                while(true) {
+                    let childNodes = node.childNodes;
+                    if(childNodes.length !== 1) {
+                        childNodesTextLengths = Array.prototype.map.call(childNodes, n => n.textContent?.length ?? 0);
+                        break;
+                    }
+                    node = childNodes[0];
+                }
+
+                let isTextNode = node.nodeType === Node.TEXT_NODE;
+
+                if(isTextNode) {
+                    totalOffset = node.textContent.length;
+                    offset = Math.min(totalOffset, inlineSize * lineCount / averageCharacterWidth / fontSize);
+                } else {
+                    totalOffset = childNodesTextLengths.length;
+
+                    let totalTextLength = childNodesTextLengths.reduce((p, l) => p + l);
+                    let increasingTextLength = childNodesTextLengths[0];
+                    let approximateTextLengthInNode = Math.min(totalTextLength, approximateTextLength - textLengthBefore);
+
+                    offset = 0;
+                    while(offset < childNodesTextLengths.length) {
+                        if(approximateTextLengthInNode >= increasingTextLength
+                        && approximateTextLengthInNode <= (increasingTextLength += childNodesTextLengths[offset + 1])) {
+                            break;
+                        } else {
+                            ++offset;
+                        }
+                    }
+                }
+
+                let maxOffset = Math.min(totalOffset, offset * stepScale + 1) | 0;
+                let minOffset = Math.min(maxOffset, Math.max(0, offset / stepScale - 1)) | 0;
+
+                let needsTestMin = true;
+
+                while(minOffset < maxOffset) {
+                    range.setEnd(node, maxOffset);
+                    if(range.getBoundingClientRect()[kBlockSize] > blockSize + blockSizeTolerance) {
+                        break;
+                    } else {
+                        minOffset = maxOffset;
+                        maxOffset = Math.min(totalOffset, maxOffset * stepScale + 1) | 0;
+                        needsTestMin = false;
+                    }
+                }
+
+                while(needsTestMin && minOffset < maxOffset) {
+                    if(minOffset === 0 && node !== testElement) {
+                        let parent = node.parentNode;
+                        range.setEnd(parent, Array.prototype.indexOf.call(parent.childNodes, node));
+                    } else {
+                        range.setEnd(node, minOffset);
+                    }
+                    if(range.getBoundingClientRect()[kBlockSize] <= blockSize + blockSizeTolerance) {
+                        break;
+                    } else {
+                        maxOffset = minOffset;
+                        minOffset = Math.max(0, minOffset / stepScale - 1) | 0;
+                    }
+                }
+
+                offset = minOffset;
+
+                while(minOffset < maxOffset) {
+                    let shouldBreak = false;
+                    if(isTextNode) {
+                        offset = Math.floor((minOffset + maxOffset) / 2);
+                        if(offset === minOffset) {shouldBreak = true;}
+                    } else {
+                        offset = Math.ceil((minOffset + maxOffset) / 2);
+                        if(offset === maxOffset) {shouldBreak = true;}
+                    }
+
+                    if(offset === 0 && node !== testElement) {
+                        let parent = node.parentNode;
+                        range.setEnd(parent, Array.prototype.indexOf.call(parent.childNodes, node));
+                    } else {
+                        range.setEnd(node, offset);
+                    }
+
+                    if(isFirstLine && range.getBoundingClientRect()[kInlineSize] > inlineSize) {
+                        inlineSize = 0;
+                        break;
+                    } 
+
+                    if(shouldBreak) {break;}
+
+                    if(range.getBoundingClientRect()[kBlockSize] <= blockSize + blockSizeTolerance) {
+                        minOffset = offset;
+                    } else {
+                        maxOffset = offset;
+                    }
+                }
+
+                if(isTextNode) {
+                    break;
+                } else {
+                    for(let i = 0; i < offset - 1; ++i) {textLengthBefore += childNodesTextLengths[i];}
+                    node = node.childNodes[Math.max(0, offset - 1)];
+                }
+            }
+        }
+
+        if(inlineSize === 0) {
+            range.detach();
+            isFirstLine = false;
+            continue;
+        }
+
+        let span = document.createElement("span");
+        span.appendChild(range.extractContents());
+        span.setAttribute("style", `display: inline-block; ${kCSSMarginInlineEnd}: ${
+            isFirstLine ? -1 - firstLineEndPadding : -1
+        }px; ${kInlineSize}: ${inlineSize}px;`);
+        fragment.appendChild(span);
+        hasAppendSegment = true;
+
+        let firstChild = testElement.firstChild;
+        if(firstChild !== null && firstChild.nodeType === Node.TEXT_NODE && firstChild.textContent === "") {
+            testElement.removeChild(firstChild);
+        } else if(firstChild === null) {
+            break;
+        }
+
+        range.detach();
+        isFirstLine = false;
+    }
+
+    element.removeChild(testElement);
+    element.appendChild(fragment);
+}
+
+makeCuttingNote.version = "0.1.0";
